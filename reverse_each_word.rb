ea950dfe09986e16dbdaba541fd084bd806aa899
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-01 22:43:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ")
+def reverse_each_word(string)
+  new_string = string.split(" ")
   reversed = []
-  new_sentence.collect {|word| reversed << word.reverse }
+  new_string.collect {|word| reversed << word.reverse }
   reversed.join(" ")
 end
